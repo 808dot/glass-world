a6cfca5a9d3d2eb387cf4861625decef72949472
Dawid <dawidolek808@gmail.com>
2023-09-15 14:45:21 +0200

    Basic Resposn

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,15 @@ function loadModalImg (ID){
 
 
 }
+
+
+let header = document.querySelector('header');
+let header_padding = header.currentStyle || window.getComputedStyle(header);
+
+console.log(header.offsetHeight);
+console.log(parseInt(header_padding.paddingTop));
+
+let root = document.querySelector(':root');
+let scrool_top = parseInt(header.offsetHeight) + 2 * parseInt(header_padding.paddingTop);
+
+root.style.setProperty('--scrool-top', scrool_top + "px");
